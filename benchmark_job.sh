0bdd2ce2c1f52181c1ed0e52adf1ea4d80d07c01
Henrik <henrik.viklander@gmail.com>
2025-11-12 13:23:37 +0100

    Adds loading module to job script

--- benchmark_job.sh
+++ benchmark_job.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH -A hpc2n2025-035
-#SBATCH --time=00:2:00
+#SBATCH --time=00:1:00
 # Send stderr of my program into <jobid>.error
 #SBATCH --error=%J.error
 
@@ -14,11 +14,19 @@
 # Clear the environment from any previously loaded modules
 ml purge  > /dev/null 2>&1
 ml foss/2021b
+module load GCC/14.3.0
+module load OpenMPI/5.0.8
+module load imkl/2025.1.0
+module load Armadillo/15.0.1
+module load Julia/1.12.1-linux-x86_64
+
+
+
 
 set -euo pipefail
 
 export RESULTS_FILE="lamp_result_test_run_1"
-export LAMP_N="${LAMP_N:-1000}"
+export LAMP_N="${LAMP_N:-500}"
 export LAMP_L3_CACHE_SIZE="${LAMP_L3_CACHE_SIZE:-7500000}"
 export OMP_NUM_THREADS="${OMP_NUM_THREADS:-1}"
 export LAMP_REPS="${LAMP_REPS:-10}"
@@ -44,6 +52,6 @@ srun --cpu-bind=cores ./c/bin/transposition.x                 $LAMP_N $LAMP_N $L
 srun --cpu-bind=cores ./c/bin/trmm.x                          $LAMP_N $LAMP_N
 
 
-srun --cpu-bind=cores ./cpp/armadillo/bench_armadillo 
+srun --cpu-bind=cores ./cpp/armadillo/bench_armadillo
 
 srun --cpu-bind=cores julia julia/main.jl
